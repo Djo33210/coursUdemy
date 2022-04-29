8f8f17e20917bd2b05e7aebd5ebab2b62c7fb7f9
Jonathan Courbin <jonathan.courbin@gmail.com>
2022-04-29 18:38:07 +0200

    suite du cours

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    $("button").click(function(){
+        $("div").animate({
+            width:"+=150px",
+            height: "+=150px",
+            opacity: 0,
+        },1000);
+
+
+    });
+
+
+});
